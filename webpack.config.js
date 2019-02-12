c565987574c1880bfef45d509f01af7c1cccf836
Selim Mahmud <selim.mahmud80@yahoo.com.au>
2019-02-12 21:43:17 +1030

    font awesome icon added

--- webpack.config.js
+++ webpack.config.js
@@ -18,9 +18,8 @@ module.exports = (env = {mode: "production"}) => {
         module: {
             rules: [
                 {test: /\.scss$/, use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]},
-                {test: /\.(png|svg|jpg|gif)$/, use: ["file-loader"]},
-                {test: /\.(woff|woff2|eot|ttf|otf)$/, use: ["file-loader"]},
-                { test: /\.html$/, loader: "html-loader" }
+                {test: /\.(png|svg|jpg|gif|svg|woff|woff2|eot|ttf|otf)$/, use: ["file-loader"]},
+                { test: /\.html$/, use: ["html-loader"] }
         ]
         },
         plugins: [
